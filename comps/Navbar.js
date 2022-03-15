909d56355fb21f4a429fbb5d20d75043bfcf995c
Abed <abedhamami42@gmail.com>
2022-03-15 15:35:21 +0200

    intive-app

---
+++ comps/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return ( 
+        <nav>
+            <div className="logo">
+                <h1>in<span className="tech">tech</span>gration</h1>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
